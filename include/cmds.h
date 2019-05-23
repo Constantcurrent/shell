1e21518defa04528d01f8a668a3f2cdbf8209897
Constantcurrent <hsyiivy@163.com>
2019-05-23 19:21:30 +0800

    create include dir

---
+++ include/cmds.h
@@ -0,0 +1,16 @@
+#ifndef _CMDS_H
+#define _CMDS_H
+
+
+
+
+
+#endif
+int cd(char **args);
+int ls(char **args);
+int redirect(char *args,char* args);
+int help(char **args);
+int exit(char **args);
+(*func[])(**args)={
+
+}
